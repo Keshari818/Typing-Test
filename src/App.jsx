1fe003374375738477843626a5fc6287fdc08f15
Harsh Keshari <harshkeshari1201@gmail.com>
2024-07-01 10:19:54 +0530

    add all file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TypingTest from "./components/TypingTest";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <TypingTest />
+      </div>
+    </>
+  )
+}
+
+export default App
